a01d47133be6775f6fe5165770cb936db52ddf4a
edwinacos55 <edwinacos55@gmail.com>
2022-05-24 22:07:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Keyboard from "./Components/Keyboard";
+import { boardDefault } from "./Components/Words";
+import GameBoard from "./Components/GameBoard";
+import {createContext, useState} from "react";
+
+export const AppContext= createContext();
 
 function App() {
+  const[board, setBoard] =useState (boardDefault); 
+  const [currentAttempt, setCurrentAttempt] = useState({attempt: 0, letterIndex: 0})
+
+  const onSelectLetter =(keyVal) => {
+    if (currentAttempt.letterIndex > 4) return;
+    const currentBoard = [...board];
+    currentBoard[currentAttempt.attempt][currentAttempt.letterIndex] = keyVal;
+    setBoard(currentBoard);
+    setCurrentAttempt({...currentAttempt, letterIndex: currentAttempt.letterIndex +1});
+  }
+  const onDelete =() =>{
+    if(currentAttempt.letterIndex=== 0) return;
+    const currentBoard = [...board];
+    currentBoard[currentAttempt.attempt][currentAttempt.letterIndex -1] = "";
+    setBoard(currentBoard)
+    setCurrentAttempt({...currentAttempt, letterIndex: currentAttempt.letterIndex -1})
+  }
+  const onEnter =() =>{
+    if(currentAttempt.letterIndex !==5) return;
+    setCurrentAttempt({attempt: currentAttempt.attempt +1, letterIndex: 0})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+      <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{board, setBoard, currentAttempt, setCurrentAttempt, onDelete, onEnter, onSelectLetter}}>
+        <div className="game">
+<GameBoard />
+<Keyboard />
+</div>
+</AppContext.Provider>
     </div>
   );
 }
